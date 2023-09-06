67759732402d484e21299a40c3073c0b50a573e4
DotheCake <marianafmollo@hotmail.com>
2023-09-05 21:56:39 -0300

    Calculo de media

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+alert("Bem vindo✨") // Comando de saida de dados
+
+console.log("Bem vindo novamente 🎇") // comando para registrar mensagem no console F12
+
+alert(10/5)
+
+console.log(10/5)
+*/
+
+N1 = 10
+N2 = 8
+N3 = 7
+N4 = 8
+
+console.log("A sua média é:")
+console.log((N1+N2+N3+N4)/4)
+
+media = (N1+N2+N3+N4)/4
+
+console.log(media)
